bbe3e07815e9cabea701ad9487dfca83c838ed4d
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-08 13:59:58 +0545

    feat:implement MyFormBloc to manage form state and events

--- lib/features/presentation/bloc/form/form_bloc.dart
+++ lib/features/presentation/bloc/form/form_bloc.dart
@@ -1,35 +1,53 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'form_event.dart';
+import 'package:bloc/bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'form_state.dart';
+import 'form_event.dart';
 
-class FormBloc extends Bloc<FormEvent, FormState> {
-  FormBloc() : super(const FormState()) {
-    on<FullNameChanged>((event, emit) {
-      emit(state.copyWith(fullName: event.fullName));
-    });
-    on<EmailChanged>((event, emit) {
-      emit(state.copyWith(email: event.email));
-    });
-    on<PasswordChanged>((event, emit) {
-      emit(state.copyWith(password: event.password));
-    });
-    on<AddressChanged>((event, emit) {
-      emit(state.copyWith(address: event.address));
-    });
-    on<GenderChanged>((event, emit) {
-      emit(state.copyWith(gender: event.gender));
-    });
-    on<FacultyChanged>((event, emit) {
-      final updatedFaculties = List<String>.from(state.faculties);
-      if (event.isSelected) {
-        updatedFaculties.add(event.faculty);
-      } else {
-        updatedFaculties.remove(event.faculty);
-      }
-      emit(state.copyWith(faculties: updatedFaculties));
-    });
-    on<SubmitForm>((event, emit) {
-      emit(state.copyWith(isSubmitted: true));
-    });
+class MyFormBloc extends Bloc<FormEvent, FormState> {
+  MyFormBloc() : super(FormState()) {
+    on<NameChanged>(_onNameChanged);
+    on<EmailChanged>(_onEmailChanged);
+    on<FormSubmitted>(_onFormSubmitted);
+
+    _loadSavedData();
+  }
+
+  void _onNameChanged(NameChanged event, Emitter<FormState> emit) {
+    emit(state.copyWith(
+        name: event.name, isValid: _validateForm(event.name, state.email)));
+  }
+
+  void _onEmailChanged(EmailChanged event, Emitter<FormState> emit) {
+    emit(state.copyWith(
+        email: event.email, isValid: _validateForm(state.name, event.email)));
+  }
+
+  Future<void> _onFormSubmitted(
+      FormSubmitted event, Emitter<FormState> emit) async {
+    emit(state.copyWith(isSubmitting: true));
+
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('name', state.name);
+      await prefs.setString('email', state.email);
+
+      emit(state.copyWith(isSubmitting: false, isSuccess: true));
+    } catch (e) {
+      emit(state.copyWith(
+          isSubmitting: false, errorMessage: 'Failed to save data'));
+    }
+  }
+
+  bool _validateForm(String name, String email) {
+    return name.isNotEmpty && email.contains('@');
+  }
+
+  Future<void> _loadSavedData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedName = prefs.getString('name') ?? '';
+    final savedEmail = prefs.getString('email') ?? '';
+
+    if (savedName.isNotEmpty) add(NameChanged(savedName));
+    if (savedEmail.isNotEmpty) add(EmailChanged(savedEmail));
   }
 }
