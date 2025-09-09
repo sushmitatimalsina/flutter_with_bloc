0f739ea7b42fd6b203b3668e4023a0fd7aee9bd7
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-09 20:05:00 +0545

    feat: update custom_text_field

--- lib/core/utils/custom_text_field.dart
+++ lib/core/utils/custom_text_field.dart
@@ -2,28 +2,21 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
-  final String? initialValue;
-  final bool obscureText;
+  final TextEditingController controller;
   final ValueChanged<String> onChanged;
 
   const CustomTextField({
     super.key,
     required this.label,
-    this.initialValue,
-    this.obscureText = false,
+    required this.controller,
     required this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      initialValue: initialValue,
-      obscureText: obscureText,
-      decoration: InputDecoration(
-        labelText: label,
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-        contentPadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-      ),
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(labelText: label, border: OutlineInputBorder()),
       onChanged: onChanged,
     );
   }
