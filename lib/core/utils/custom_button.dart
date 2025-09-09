d41e09bc501b0e7c7615161e1a3af4aa7d99ad17
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-09 20:03:26 +0545

    feat: update custom_button

--- lib/core/utils/custom_button.dart
+++ lib/core/utils/custom_button.dart
@@ -4,20 +4,11 @@ class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
 
-  const CustomButton({
-    super.key,
-    required this.text,
-    required this.onPressed,
-  });
+  const CustomButton({super.key, required this.text, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
-        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
       onPressed: onPressed,
       child: Text(text),
     );
--- lib/core/utils/string_const.dart
+++ lib/core/utils/string_const.dart
@@ -2,3 +2,10 @@ const String  failedToFetchPosts="Failed to fetch posts";
 const String  posts="Posts";
 
 const String  noData="No data";
+const String  Name="Name";
+const String  Email="Email";
+const String  Submit="Submit...";
+
+
+
+
